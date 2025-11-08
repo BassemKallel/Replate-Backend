36104f0c2e6c8b2618168c394b1ce3f3c2b5d834
Bassem Kallel <kallelbassem@bizerte.r-iset.tn>
2025-11-09 00:50:17 +0100

    initiation

---
+++ src/main/java/com/replate/replatebackend/ReplateBackendApplication.java
@@ -0,0 +1,13 @@
+package com.replate.replatebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReplateBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReplateBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/replate/replatebackend/ReplateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.replate.replatebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReplateBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
